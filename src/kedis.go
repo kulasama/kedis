463bec1a2330b5f68184438ede98a19533cc725d
kula <kulasama@gmail.com>
2010-12-08 18:20:49 +0800

    add first server model with goruntine

--- src/kedis.go
+++ src/kedis.go
@@ -5,16 +5,26 @@ import (
         "net"
        )   
 
-func Serve(c net.Conn) {
+func RequestHandler(conn net.Conn) {  
+    defer conn.Close() 
+    b:=[]byte("hello world")
+    conn.Write(b)
 }
 
 func main() {
-    fmt.Printf("hello world\n") 
+    fmt.Printf("server init\n") 
     listen, err := net.Listen("tcp","127.0.0.1:8080")  
     if err != nil {
         fmt.Printf("server fault",err)
         return 
-    }
-    conn, err := listen.Accept()
-    fmt.Printf("conected",conn,err,"\n")
+    }                           
+    for {
+        conn, err := listen.Accept()  
+        if err != nil {
+            fmt.Printf("accept error",err)
+            //break
+        }
+        go RequestHandler(conn)
+        fmt.Printf("conected",conn.RemoteAddr(),"\n")  
+    }           
 }
